f09b0c041e497c1278cfdff29dc1f4c77fd055fe
Klabenedict <57775781+Klabenedict@users.noreply.github.com>
2019-11-15 17:14:14 -0800

    Create bennettCode.py

---
+++ bennettCode.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+
+danielsFirstWords = ("My dad does stuff. ")
+williamsFirstWords = ("No thank you, I'm vegetarian. ")
+print danielsFirstWords+williamsFirstWords
+print williamsFirstWords+danielsFirstWords
